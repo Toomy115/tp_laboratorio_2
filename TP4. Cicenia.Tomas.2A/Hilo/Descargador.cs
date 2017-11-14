dfa0efbfe8b80dcb6b0b42939a5b0e81b4c0b3a5
TOMAS-PC\tomas <tomascicenia65@gmail.com>
2017-11-13 21:56:32 -0300

    Agrego TP4

---
+++ TP4. Cicenia.Tomas.2A/Hilo/Descargador.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Net; // Avisar del espacio de nombre
+using System.ComponentModel;
+
+namespace Hilo
+{
+    public class Descargador
+    {
+        private string html;
+        private Uri direccion;
+        private int progreso;
+
+        public string HTML { get { return this.html; } }
+
+        public int Progreso
+        {
+            get { return this.progreso; }
+            set { this.progreso = value; }
+        }
+
+
+        public Descargador(Uri direccion)
+        {
+            this.html = "";
+            this.direccion = direccion;
+            this.Progreso = 0;
+        }
+
+        public void IniciarDescarga()
+        {
+            try
+            {
+                WebClient cliente = new WebClient();
+                cliente.DownloadProgressChanged += new DownloadProgressChangedEventHandler(WebClientDownloadProgressChanged);
+                cliente.DownloadStringCompleted += new DownloadStringCompletedEventHandler(WebClientDownloadCompleted);
+
+                cliente.DownloadStringAsync(direccion);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        private void WebClientDownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            this.Progreso = e.ProgressPercentage;
+        }
+        private void WebClientDownloadCompleted(object sender, DownloadStringCompletedEventArgs e)
+        {
+            this.html = e.Result ?? "Sitio incorrecto";
+        }
+    }
+}
---
+++ TP4. Cicenia.Tomas.2A/Navegador/frmHistorial.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Navegador
+{
+    public partial class frmHistorial : Form
+    {
+        public const string ARCHIVO_HISTORIAL = "historico.dat";
+
+        public frmHistorial()
+        {
+            InitializeComponent();
+        }
+
+        private void frmHistorial_Load(object sender, EventArgs e)
+        {
+            List<string> list;
+
+            Archivos.Texto archivos = new Archivos.Texto(frmHistorial.ARCHIVO_HISTORIAL);
+
+            if(archivos.leer(out list) == false)
+            {
+                MessageBox.Show("Error al cargar archivo");
+                this.Close();
+            }
+
+            foreach(string s in list)
+            {
+                this.lstHistorial.Items.Add(s);
+            }
+            
+        }
+    }
+}
