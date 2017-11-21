1b7a08dbc27581d06f33b78e99fd41b046af7439
TOMAS-PC\tomas <tomascicenia65@gmail.com>
2017-11-21 14:25:56 -0300

    TP4 Corregido

---
+++ RecuperatoriosTP/TP4/Archivos/Texto.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Archivos
+{
+    public class Texto : IArchivo<string>
+    {
+        string path;
+        public Texto(string archivo)
+        {
+            this.path = archivo;
+        }
+
+        public bool guardar(string datos)
+        {
+            try
+            {
+                StreamWriter streamWriter = new StreamWriter(this.path, true);
+                streamWriter.WriteLine(datos);
+                streamWriter.Close();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public bool leer(out List<string> datos)
+        {
+            datos = new List<string>();
+            try
+            {
+                
+                StreamReader streamReader = new StreamReader(this.path);
+                while(streamReader.EndOfStream == false)
+                {
+                    datos.Add(streamReader.ReadLine());
+                }
+                streamReader.Close();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
